502fb0dc6782449038346ead865fc97b3b0db85d
Patrik <psirb@myuax.com>
2024-02-07 19:03:24 +0100

    Ejercicio 1 hecho y comentado.

--- src/ejercicio3.java
+++ src/ejercicio3.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 
-class Publicacion{
+class Publicacion{      //Necesitamos una clase para representar las publicaciones de las rrss.
     String texto;
 
     public Publicacion(String texto) {
@@ -15,6 +15,7 @@ class Publicacion{
     }
 }
 public class ejercicio3 {
+
     private static final double UMBRAL_POS = 1;
     private static final double UMBRAL_NEG = -1;
     private static final List<String> palabrasPos = Arrays.asList("feliz", "genial", "fantástico", "positivo", "amor");
@@ -48,6 +49,9 @@ public class ejercicio3 {
             }
         }
 
+        //Si el contador de palabras positivas es mayor que las negativas, devuelve 1, si es al contrario, devuelve -1 y
+        //si no devuelve por defecto un 0
+
         if (contadorPos > contadorNeg){
             return 1;
         } else if (contadorNeg > contadorPos) {
@@ -58,12 +62,16 @@ public class ejercicio3 {
 
     }
 
+    //Creamos un metodo que cree una lista de objetos Publicacion y lo recorremos con un for-each.
+    //Llamamos al metodo analizarSentimiento para analizar el texto de la publicacion y luego imprimimos los resultados.
+
     public void analizarPublicaciones(List<Publicacion> publicaciones){
         for (Publicacion publicacion : publicaciones){
             String sentimiento = analizarSentimiento(publicacion.texto);
             System.out.println("Publicacion: " + publicacion.texto + " - " + "Sentimiento: " + sentimiento);
         }
     }
+
 
     public static void main(String[] args) {
 
